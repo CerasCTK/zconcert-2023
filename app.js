9ce43ee83ba334d6eec6a60a37b7abd67f7ac1bd
dainsd382 <nguyenducdai382@gmail.com>
2023-07-15 22:30:23 +0700

    Updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Scroll Navbar
 const navbarElement = document.getElementById("intro-navbar");
 
 window.onscroll = () => {
@@ -7,3 +8,28 @@ window.onscroll = () => {
     navbarElement.classList.remove("bg-scroll-navbar");
   }
 };
+
+// Show video section History
+function toggleVideo(index) {
+  var iframe = document.getElementById("video" + index);
+  if (iframe.style.display === "none") {
+    iframe.style.display = "block";
+  } else {
+    iframe.style.display = "none";
+  }
+}
+
+// Play music when hover Vocal img
+const hoverImages = document.querySelectorAll(".hover-image");
+const hoverMusics = document.querySelectorAll(".hover-music");
+
+hoverImages.forEach((image, index) => {
+  image.addEventListener("mouseover", function () {
+    hoverMusics[index].play();
+  });
+
+  image.addEventListener("mouseout", function () {
+    hoverMusics[index].pause();
+    hoverMusics[index].currentTime = 0; // Resets the audio to the beginning
+  });
+});
