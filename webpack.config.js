4286961711f27c0bcf6ebf934aa278f7c61dc254
Ceras <ceras410@gmail.com>
2023-07-20 01:37:33 +0700

    updated project

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g)$/i,
+        test: /\.(png|jpe?g|webp)$/i,
         type: "asset/resource",
         use: [
           {
