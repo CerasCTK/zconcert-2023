5c91cf275ae0a67a0af0c875d192bc25fd7f965f
dainsd382 <nguyenducdai382@gmail.com>
2023-07-23 20:10:51 +0700

    updated

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -1,4 +1,11 @@
 import { Popup } from "./utils/popup/popup-utils";
 
 const startup = document.getElementById("startup");
-new Popup.PopupBuilder(startup).showOnLoad().closeOnClickOutside().build();
+const startupPopup = new Popup.PopupBuilder(startup)
+  .showOnLoad()
+  .closeOnClickOutside()
+  .build();
+
+startupPopup.wrapper.style.backgroundImage =
+  "url(../assets/img/background/startup.jpg)";
+startupPopup.wrapper.style.backgroundSize = "cover";
