55626f1dfcad7bf6bbd7d70818a586051091088a
Ceras <ceras410@gmail.com>
2023-07-23 20:13:43 +0700

    Added background to startup popup

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -1,11 +1,12 @@
 import { Popup } from "./utils/popup/popup-utils";
 
+import startupImg from "../assets/img/background/startup.jpg";
+
 const startup = document.getElementById("startup");
 const startupPopup = new Popup.PopupBuilder(startup)
   .showOnLoad()
   .closeOnClickOutside()
   .build();
 
-startupPopup.wrapper.style.backgroundImage =
-  "url(../assets/img/background/startup.jpg)";
+startupPopup.wrapper.style.backgroundImage = `url(${startupImg})`;
 startupPopup.wrapper.style.backgroundSize = "cover";
