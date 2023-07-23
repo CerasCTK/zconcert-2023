86ec8a68464f62d3302e361279eda3d77f48e60a
dainsd382 <nguyenducdai382@gmail.com>
2023-07-23 22:25:18 +0700

    updated

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -1,7 +1,10 @@
 import { Popup } from "./utils/popup/popup-utils";
+import { imgBgPopup } from "./assets";
 
 const startup = document.getElementById("startup");
 const startupPopup = new Popup.PopupBuilder(startup)
   .showOnLoad()
   .closeOnClickOutside()
   .build();
+startupPopup.wrapper.style.backgroundImage = `url(${imgBgPopup})`;
+startupPopup.wrapper.style.backgroundSize = "cover";
