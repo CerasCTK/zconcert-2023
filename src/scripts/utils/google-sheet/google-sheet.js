2e49d82ab4091006bfc036a88211b1f87346c284
Ceras <ceras410@gmail.com>
2023-07-24 20:27:03 +0700

    Added DateTime column to google-sheet

--- src/scripts/utils/google-sheet/google-sheet.js
+++ src/scripts/utils/google-sheet/google-sheet.js
@@ -3,7 +3,10 @@ import Sheet2API from "sheet2api-js/src/main";
 const url = "https://sheet2api.com/v1/VbuiSNzpQ6eR/regulartickets";
 
 export const createData = (fullName, phoneNumber, email, facebook, numOfLotus, numOfJasmine, numOfIris, totalPrice, paymentMethod) => {
+  const currentDate = new Date();
+  const dateTime = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear() + " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" +  currentDate.getSeconds();
   return {
+    DateTime: dateTime,
     FullName: fullName,
     PhoneNumber: phoneNumber,
     Email: email,
